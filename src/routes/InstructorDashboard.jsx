2b38117ea34aa5e012c4e7bec4fb0fd4f700b292
Suraj Yadav <surajyadavy676@gmail.com>
2024-05-12 16:56:32 +0530

    add text

--- src/routes/InstructorDashboard.jsx
+++ src/routes/InstructorDashboard.jsx
@@ -4,6 +4,7 @@ function InstructorDashboard() {
   return (
     <div>
       <h1 className='text-center text-xl font-bold'>Instructor Dashboard</h1>
+      <h2 className='text-center my-6 text-green-300 text-lg'>List of All Lecture </h2>
     </div>
   )
 }
